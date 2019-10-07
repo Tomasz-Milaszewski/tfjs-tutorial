48aeafd3bbd9bb7555575f42e14ad5b9304abb69
Tomek_M <tomasz.milaszewski@gmail.com>
2019-10-07 21:11:00 +0200

    Add some more tensor examples

--- src/tensors/index.js
+++ src/tensors/index.js
@@ -1,16 +1,19 @@
 import * as tf from '@tensorflow/tfjs';
 // Create a rank-2 tensor (matrix) matrix tensor from a multidimensional array.
-// const a = tf.tensor([[1, 2], [3, 4]]);
-// console.log('shape:', a.shape);
-// a.print();
+const a = tf.tensor([[1, 2], [3, 4, 5, 6], [7, 8]]);
+console.log('rank:', a.rank);
+console.log('shape:', a.shape);
+a.print();
 
 // Or you can create a tensor from a flat array and specify a shape.
-const shape = [2, 2];
+const shape = [4, 1];
 const b = tf.tensor([1, 2, 3, 4], shape);
+console.log('rank:', b.rank);
 console.log('shape:', b.shape);
 b.print();
 
-const a = tf.tensor([[1, 2], [3, 4]], [2, 2], 'int32');
-console.log('shape:', a.shape);
-console.log('dtype', a.dtype);
-a.print();
+const c = tf.tensor([[1, 2, 3], [3, 4, 5]], [2, 3], 'int32');
+console.log('rank:', c.rank);
+console.log('shape:', c.shape);
+console.log('dtype', c.dtype);
+c.print();
