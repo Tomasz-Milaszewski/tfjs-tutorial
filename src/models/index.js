8b0a76c1072a0dd7f07d64bf4943aae2dfddf721
Tomek_M <tomasz.milaszewski@gmail.com>
2019-10-08 19:42:38 +0200

    Define a simple sequential model via add method

--- src/models/index.js
+++ src/models/index.js
@@ -11,3 +11,9 @@ const model = tf.sequential({
  // No of params for each layer is:
  // layer one: 784(inputShape) * 32(units) + 32 = 25120
  // layer two: 32(units of previous layer) * 10(units) + 10 = 330
+
+ //Create a sequential model via add() method
+const model2 = tf.sequential();
+model2.add(tf.layers.dense({inputShape: [784], units: 32, activation: 'relu'})); 
+model2.add(tf.layers.dense({units: 10, activation: 'softmax'}));
+
