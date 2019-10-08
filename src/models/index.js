58303e473d6488d33b1a0fa9ef99c8b4997d91b6
Tomek_M <tomasz.milaszewski@gmail.com>
2019-10-08 19:45:08 +0200

    Define the same model via tf.model with arbitrary layers

--- src/models/index.js
+++ src/models/index.js
@@ -17,3 +17,9 @@ const model2 = tf.sequential();
 model2.add(tf.layers.dense({inputShape: [784], units: 32, activation: 'relu'})); 
 model2.add(tf.layers.dense({units: 10, activation: 'softmax'}));
 
+// Create the same model with an arbitrary graph of layers, by connecting them via the apply() method.
+const input = tf.input({shape: [784]});
+const dense1 = tf.layers.dense({units: 32, activation: 'relu'}).apply(input);
+const dense2 = tf.layers.dense({units: 10, activation: 'softmax'}).apply(dense1);
+const model3 = tf.model({inputs: input, outputs: dense2});
+model3.summary();
